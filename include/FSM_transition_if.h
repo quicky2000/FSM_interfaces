38582898c716b6f24a843ff511fe2baee138b3f5
Julien Thevenon <julien_thevenon@yahoo.fr>
2013-09-13 18:25:10 +0200

    Remove namespace

--- include/FSM_transition_if.h
+++ include/FSM_transition_if.h
@@ -3,15 +3,14 @@
 #define _FSM_TRANSITION_IF_
 
 #include <string>
-using namespace std;
 
 class FSM_transition_if
 {
-	public:
-		// Methods to implement
-		virtual string toString(void)const=0;
-
-		// Virtual destructor
-		virtual ~FSM_transition_if(void);
+ public:
+  // Methods to implement
+  virtual std::string toString(void)const=0;
+  
+  // Virtual destructor
+  virtual ~FSM_transition_if(void);
 };
 #endif
