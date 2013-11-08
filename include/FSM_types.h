9784cedaaba9896a49d915dfdf4edcae38db9b05
Julien Thevenon <julien_thevenon@yahoo.fr>
2013-11-08 17:15:59 +0100

    Adding GPL headers and licence

--- include/FSM_types.h
+++ include/FSM_types.h
@@ -15,32 +15,18 @@
       You should have received a copy of the GNU General Public License
       along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
-#ifndef _FSM_UI_BASE_H_
-#define _FSM_UI_BASE_H_
-
-#include <string>
-#include <map>
+#ifndef _FSM_TYPES_
+#define _FSM_TYPES_
 
 namespace FSM_interfaces
 {
-  class FSM_situation_if;
-
-  class FSM_UI_if
+  class FSM_types
   {
   public:
-    // Methods to implement
-    virtual void display_situation(const FSM_situation_if & p_situation)=0;
-    virtual const std::string & get_class_name(void)const=0;
-  
-    // Virtual destructor
-    inline virtual ~FSM_UI_if(void){}
+    typedef unsigned int transition_weight_t;
+    typedef unsigned int transition_index_t;
+  private:
   };
-
-  typedef FSM_UI_if & (*FSM_UI_creator_t)();
-
-  void register_fsm_ui(const std::string & p_fsm_name,
-		       FSM_UI_creator_t p_creator,
-		       std::map<std::string,FSM_UI_creator_t> & p_factory);
 }
 #endif
 //EOF
